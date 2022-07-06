766243ff59a872b197bf347e38337b3011080403
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-06 22:32:27 +0100

    Add missing variables

--- variables.tf
+++ variables.tf
@@ -8,3 +8,46 @@ variable "heroku_api_key" {
   type      = string
   sensitive = true
 }
+
+variable "github_client_id" {
+  default = ""
+  type    = string
+}
+
+variable "prometheus_uri" {
+  default = ""
+  type    = string
+}
+
+variable "prometheus_user" {
+  default = ""
+  type    = string
+}
+
+variable "grafana_uri" {
+  default = ""
+  type    = string
+}
+
+variable "github_client_secret" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+variable "github_pat" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+
+variable "prometheus_token" {
+  default   = ""
+  type      = string
+  sensitive = true
+}
+
+variable "grafana_token" {
+  default   = ""
+  type      = string
+  sensitive = true
+}

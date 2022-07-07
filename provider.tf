185aaee30d8b18f54b719a33281f715e47b64c1e
Matthew Macdonald-Wallace <matt@doics.co>
2022-07-07 13:40:37 +0100

    Add HerokuX provider

--- provider.tf
+++ provider.tf
@@ -7,8 +7,23 @@ terraform {
   }
 }
 
-# Configure the Heroku provider
+terraform {
+  required_providers {
+    herokux = {
+      source  = "davidji99/herokux"
+      version = "0.33.4"
+    }
+  }
+}
+
+# Configure the Primary Heroku provider
 provider "heroku" {
   email   = var.heroku_account_email
   api_key = var.heroku_api_key
 }
+
+# Configure the HerokuX provider for container management etc
+provider "herokux" {
+  api_key = var.heroku_api_key
+}
+
